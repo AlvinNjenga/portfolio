25bef7f3c9b8079cc190bb9832acbdd9e9b11642
Alvin N <109256320+AlvinNjenga@users.noreply.github.com>
2025-02-15 10:03:36 +0300

    Add container and logo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./components/Navigation";
+
+const App = () => {
+  return (
+    <div className="container w-full max-w-screen-lg min-h-screen absolute top-0 left-[50%] -translate-x-[50%] px-4 py-0 overflow-hidden">
+      <Navigation />
+    </div>
+  )                  
+}
+
+export default App;
